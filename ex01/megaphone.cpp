0c3bf08249dc5eeefabbd96526e7398194064b6b
lilia10010 <25871372+Lilia10010@users.noreply.github.com>
2025-12-02 23:15:26 -0300

    add initial implementation of megaphone program with Makefile

---
+++ ex01/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cctype>
+#include <cstring>
+
+void megaphone(int argc, char **argv)
+{
+    for(int i = 1; i < argc; ++i)
+    {
+        std::size_t size = std::strlen(argv[i]);
+        for(int j = 0; j < (int)size; ++j)
+        {
+            std::cout << (char)std::toupper(argv[i][j]);
+        }
+    }
+    std::cout << std::endl;
+}
+
+
+int main(int argc, char **argv)
+{
+  if (argc <= 1)
+  {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return (0);
+  }
+  megaphone(argc, argv);
+  return (0);
+}
